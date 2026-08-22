a87e56b32d25037c78fd1e4a49ff1e62935115dd
unazikx <xfalwa@gmail.com>
2026-08-22 17:35:46 +0900

    flake: refactor

--- homeManager/cava.nix
+++ homeManager/cava.nix
@@ -9,19 +9,17 @@ let
 
   base16 = type: config.matugen.theme.generated.base16.${type}.default;
 
-  mkColorsAttrs =
-    colorsList:
-    (
-      (builtins.listToAttrs (
-        lib.imap0 (
-          number: color: lib.nameValuePair "gradient_color_${toString (number + 1)}" "'#${color}'"
-        ) colorsList
-      ))
-      // {
-        gradient = 1;
-        gradient_count = builtins.length colorsList;
-      }
-    );
+  pair = (pos: color: lib.nameValuePair "gradient_color_${toString (pos + 1)}" "'${color}'");
+
+  colorsList = [
+    (base16 "base0e")
+    (base16 "base0d")
+    (base16 "base0c")
+    (base16 "base0b")
+    (base16 "base0a")
+    (base16 "base09")
+    (base16 "base08")
+  ];
 in
 
 {
@@ -35,14 +33,14 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.cava = {
-      settings.color = mkColorsAttrs [
-        (base16 "base0e")
-        (base16 "base0d")
-        (base16 "base0c")
-        (base16 "base0b")
-        (base16 "base0a")
-        (base16 "base09")
-        (base16 "base08")
+      settings.color = lib.mkMerge [
+        (builtins.listToAttrs (lib.imap0 pair colorsList))
+        {
+          background = base16 "base00";
+          foreground = base16 "base05";
+          gradient = 1;
+          gradient_count = builtins.length colorsList;
+        }
       ];
     };
   };
---
+++ readme.md
@@ -0,0 +1,17 @@
+# List of templates for Matugen-Nix
+
+1. take template:
+   - from [iniox templates](https://github.com/InioX/matugen-themes#templates-for-programs)
+   - or adapt from [stylix](https://github.com/nix-community/stylix/tree/master/modules)
+   - or write urself
+
+2. test:
+   - !!! NO I WONT ANY _INPUTS_ IN THIS FLAKE !!!
+
+3. commit, push and pull request
+
+4. wait
+
+5. happy happy
+
+![why are u hovered me? are u gay?](https://media.tenor.com/lCKwsD2OW1kAAAAj/happy-cat-happy-happy-cat.gif)
