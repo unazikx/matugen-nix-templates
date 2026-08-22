aed85ce5e98f90437ba29757414bbdac974137a4
unazikx <xfalwa@gmail.com>
2026-08-22 21:50:33 +0900

    import-tree: fix

--- import-tree.nix
+++ import-tree.nix
@@ -1,9 +1,30 @@
 path:
 
 let
-  allFiles = builtins.attrNames (builtins.readDir path);
-  filteredFiles = builtins.filter (name: builtins.substring 0 1 name != "_") allFiles;
-  imported = map (name: import "${path}/${name}") filteredFiles;
+  traverse =
+    dir:
+    let
+      items = builtins.attrNames (builtins.readDir dir);
+      process =
+        name:
+        let
+          full = "${dir}/${name}";
+          type = (builtins.readDir dir).${name};
+        in
+        if ((type == "directory") && (builtins.substring 0 1 name != "_")) then
+          traverse full
+        else if
+          (
+            (type == "regular")
+            && (builtins.match ".*\\.nix$" name != null)
+            && (builtins.substring 0 1 name != "_")
+          )
+        then
+          [ (import full) ]
+        else
+          [ ];
+    in
+    builtins.concatMap process items;
 in
 
-imported
+traverse path
