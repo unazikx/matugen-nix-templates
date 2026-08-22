2cd9a1611d50a9d1564850a621041b74259c55bc
unazikx <xfalwa@gmail.com>
2026-08-22 21:47:33 +0900

    utils: improve palette

--- homeManager/utils/palette.nix
+++ homeManager/utils/palette.nix
@@ -7,14 +7,26 @@
 let
   cfg = config.matugen.palette;
   theme = config.matugen.theme;
+
+  inherit (config.matugen.theme.generated)
+    colors
+    ;
 in
 
 {
   config = lib.mkIf cfg.enable {
-    xdg.cacheFile = {
-      "matugen/theme.json".text = lib.toJSON theme;
-      "matugen/base16.json".text = lib.toJSON theme.base16;
-      "matugen/palettes.json".text = lib.toJSON theme.palettes;
+    xdg.configFile = {
+      "matugen-nix/theme.json".text = lib.toJSON theme;
+      "matugen-nix/base16.json".text = lib.toJSON theme.base16;
+      "matugen-nix/palettes.json".text = lib.toJSON theme.palettes;
+
+      "matugen-nix/colors.css".text = ''
+        :root {
+        ${lib.concatStringsSep "\n" (
+          lib.mapAttrsToList (name: value: "  --${name}: ${value.default};") colors
+        )}
+        }
+      '';
     };
   };
 }
