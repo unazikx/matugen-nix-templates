ff86839698130c2743e7710dcac47bed9db8746b
unazikx <xfalwa@gmail.com>
2026-08-22 21:47:39 +0900

    import-tree: fix

--- import-tree.nix
+++ import-tree.nix
@@ -2,20 +2,8 @@ path:
 
 let
   allFiles = builtins.attrNames (builtins.readDir path);
-
   filteredFiles = builtins.filter (name: builtins.substring 0 1 name != "_") allFiles;
-
-  traverse = builtins.foldl' (
-    acc: name:
-    let
-      fullPath = "${path}/${name}";
-      type = builtins.typeOf (import fullPath);
-    in
-    if ((type == "lambda") || (type == "set") || (type == "path")) then
-      (acc ++ [ (import fullPath) ])
-    else
-      acc
-  ) [ ] filteredFiles;
+  imported = map (name: import "${path}/${name}") filteredFiles;
 in
 
-traverse
+imported
